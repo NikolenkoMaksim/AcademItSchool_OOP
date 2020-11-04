9f6e1596932ea35456f5f7c0d9fa668c9bd0d9c3
NikolenkoMaksim <kinik33@mail.ru>
2020-11-04 18:50:32 +0300

    Выполнена задача List

--- List/src/singly_lincked_list/SinglyLinkedList.java
+++ List/src/singly_lincked_list/SinglyLinkedList.java
@@ -6,16 +6,11 @@ public class SinglyLinkedList<T> {
     private ListItem<T> head;
     private int length;
 
-    public SinglyLinkedList(ListItem<T> head) {
-        this.head = head;
-        length = 1;
-    }
-
     public SinglyLinkedList(T[] array) {
-        ListItem<T> previousItem = new ListItem<T>(array[array.length-1]);
+        ListItem<T> previousItem = new ListItem<>(array[array.length - 1]);
 
-        for(int i = array.length - 2; i >= 0; i--) {
-            previousItem = new ListItem<T>(array[i], previousItem);
+        for (int i = array.length - 2; i >= 0; i--) {
+            previousItem = new ListItem<>(array[i], previousItem);
         }
 
         head = previousItem;
@@ -23,24 +18,32 @@ public class SinglyLinkedList<T> {
     }
 
     public SinglyLinkedList(T data) {
-        head = new ListItem<T>(data);
+        head = new ListItem<>(data);
         length = 1;
-    }
-
-    public ListItem<T> getHead() {
-        return head;
-    }
-
-    public void setHead(ListItem<T> head) {
-        this.head = head;
     }
 
     public int getLength() {
         return length;
     }
 
-    public void setLength(int length) {
-        this.length = length;
+    private void checkIndex(int index) {
+        if (index >= length) {
+            throw new IndexOutOfBoundsException("index value " + index + " >= list length (" + length + ")");
+        }
+
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("index value " + index + " < 0");
+        }
+    }
+
+    private ListItem<T> getIndexItem(int index) {
+        ListItem<T> item = head;
+
+        for (int i = 0; i < index; i++) {
+            item = item.getNext();
+        }
+
+        return item;
     }
 
     @Override
@@ -51,7 +54,7 @@ public class SinglyLinkedList<T> {
 
         ListItem<T> next = head;
 
-        while(next != null) {
+        while (next != null) {
             stringBuilder.append(next.getData()).append(", ");
             next = next.getNext();
         }
@@ -59,5 +62,113 @@ public class SinglyLinkedList<T> {
         stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}");
 
         return stringBuilder.toString();
+    }
+
+    public T getHeadData() {
+        return head.getData();
+    }
+
+    public T getIndexItemData(int index) {
+        checkIndex(index);
+
+        return getIndexItem(index).getData();
+    }
+
+    public T setIndexItemData(int index, T data) {
+        checkIndex(index);
+
+        ListItem<T> item = getIndexItem(index);
+        T oldData = item.getData();
+        item.setData(data);
+
+        return oldData;
+    }
+
+    public T deleteHead() {
+        T data = head.getData();
+        head = head.getNext();
+        --length;
+
+        return data;
+    }
+
+    public T deleteIndexItem(int index) {
+        checkIndex(index);
+
+        if (index == 0) {
+            return deleteHead();
+        } else {
+            ListItem<T> item = getIndexItem(index - 1);
+            T data = item.getNext().getData();
+            item.setNext(item.getNext().getNext());
+            --length;
+
+            return data;
+        }
+    }
+
+    public void addToHead(T data) {
+        head = new ListItem<>(data, head);
+        ++length;
+    }
+
+    public void addIndexItem(int index, T data) {
+        checkIndex(index);
+
+        if (index == 0) {
+            addToHead(data);
+        } else {
+            ListItem<T> item = getIndexItem(index - 1);
+            item.setNext(new ListItem<>(data, item.getNext()));
+            ++length;
+        }
+    }
+
+    public boolean deleteValue(T value) {
+        if (head.getData() == value) {
+            deleteHead();
+            return true;
+        }
+
+        for (ListItem<T> i = head; i.getNext() != null; i = i.getNext()) {
+            if (i.getNext().getData() == value) {
+                i.setNext(i.getNext().getNext());
+                --length;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void invert() {
+        ListItem<T> previousItem = head;
+        ListItem<T> nextItem = head.getNext();
+        head.setNext(null);
+
+        ListItem<T> i = nextItem;
+
+        while (i != null) {
+            nextItem = i.getNext();
+            i.setNext(previousItem);
+            previousItem = i;
+            i = nextItem;
+        }
+
+        head = previousItem;
+    }
+
+    public SinglyLinkedList<T> getCopy() {
+        SinglyLinkedList<T> newList = new SinglyLinkedList<>(head.getData());
+        ListItem<T> previousItem = newList.head;
+
+        for (ListItem<T> i = head.getNext(); i != null; i = i.getNext()) {
+            previousItem.setNext(new ListItem<>(i.getData()));
+            ++newList.length;
+            previousItem = previousItem.getNext();
+        }
+
+        return newList;
     }
 }
--- List/src/singly_list_main/SinglyListMain.java
+++ List/src/singly_list_main/SinglyListMain.java
@@ -4,8 +4,55 @@ import singly_lincked_list.SinglyLinkedList;
 
 public class SinglyListMain {
     public static void main(String[] args) {
-        Integer[] data = new Integer[]{0, 1, 2, 3, 4};
-        SinglyLinkedList<Integer> list1 = new SinglyLinkedList<Integer>(data);
+        Integer[] data = new Integer[]{0, null, 2, 3, 4};
+        SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>(data);
         System.out.println(list1);
+
+        System.out.println(list1.getIndexItemData(2));
+
+        System.out.println(list1.getHeadData());
+
+        System.out.println(list1.setIndexItemData(1, 1));
+        System.out.println(list1);
+
+        System.out.println(list1.deleteIndexItem(1));
+        System.out.println(list1);
+
+        list1.addToHead(-1);
+        System.out.println(list1);
+
+        System.out.println(list1.deleteIndexItem(0));
+        System.out.println(list1);
+
+        list1.addIndexItem(1, 1);
+        System.out.println(list1);
+
+        list1.addIndexItem(1, 2);
+        System.out.println(list1);
+
+        list1.addIndexItem(0, 5);
+        System.out.println(list1);
+
+        System.out.println(list1.deleteValue(4));
+        System.out.println(list1.deleteValue(9));
+        System.out.println(list1.deleteValue(5));
+        System.out.println(list1.deleteValue(2));
+        System.out.println(list1);
+        System.out.println(list1.getLength());
+
+        SinglyLinkedList<Integer> list2 = list1.getCopy();
+        System.out.println(list2);
+        System.out.println(list1.getLength());
+
+        list1.invert();
+        System.out.println(list1);
+        System.out.println(list2);
+
+        list2.setIndexItemData(1, 5);
+        System.out.println(list1);
+        System.out.println(list2);
+
+        SinglyLinkedList<String> list3 = new SinglyLinkedList<>(new String[]{"Hello,", "it", "is", "me"});
+        System.out.println(list3);
     }
 }
