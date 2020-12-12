730acede1bf68d7a09530d81bb494e32eda51148
NikolenkoMaksim <kinik33@mail.ru>
2020-12-12 14:17:11 +0300

    Внесены правки

--- CsvToHtmlConversion/src/ru/oop/nikolenko/conversion_test/ConversionTest.java
+++ CsvToHtmlConversion/src/ru/oop/nikolenko/conversion_test/ConversionTest.java
@@ -5,14 +5,17 @@ import java.io.FileNotFoundException;
 import static ru.oop.nikolenko.csv_to_html_conversion.CsvToHtmlConversion.convertCsvToHtml;
 
 public class ConversionTest {
-    public static void main(String[] args) throws FileNotFoundException {
-        if (args.length == 2) {
+    public static void main(String[] args) {
+        try {
             convertCsvToHtml(args[0], args[1]);
-        } else {
-            System.out.println("Ошибка! Аргументы программы не переданы или их количество неверно.");
-            System.out.println("В программу должно быть передано два аргумента:");
-            System.out.println("- адрес файла с исходной CSV-таблицей;");
-            System.out.println("- адрес файла для итогового HTML-кода.");
+            System.out.println("Conversion complete");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error. Program arguments weren't passed or their number is incorrect");
+            System.out.println("Two arguments must be passed to the program:");
+            System.out.println("- source CSV table file address;");
+            System.out.println("- resulting HTML code file address.");
+        } catch (FileNotFoundException e) {
+            System.out.println("source CSV table file not found: " + e.getMessage());
         }
     }
 }
