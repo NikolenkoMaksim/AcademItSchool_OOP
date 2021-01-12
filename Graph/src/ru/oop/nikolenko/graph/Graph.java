72ad5bebd68116ca04addb51abde4fc63b572348
NikolenkoMaksim <kinik33@mail.ru>
2021-01-12 23:12:57 +0700

    Исправлены замечания

---
+++ Graph/src/ru/oop/nikolenko/graph/Graph.java
@@ -0,0 +1,113 @@
+package ru.oop.nikolenko.graph;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.function.Consumer;
+
+public class Graph {
+    private final int[][] edgesMatrix;
+
+    public Graph(int[][] edgesMatrix) {
+        if (edgesMatrix == null) {
+            throw new IllegalArgumentException("edgesMatrix is null");
+        }
+
+        if (edgesMatrix.length != edgesMatrix[0].length) {
+            throw new IllegalArgumentException("edgesMatrix not square");
+        }
+
+        for (int i = 0; i < edgesMatrix.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == j) {
+                    if (edgesMatrix[i][j] != 0) {
+                        throw new IllegalArgumentException("edgesMatrix[" + i + "][" + j + "] must be 0");
+                    }
+
+                    continue;
+                }
+
+                if (edgesMatrix[i][j] != edgesMatrix[j][i]) {
+                    throw new IllegalArgumentException("edgesMatrix[" + i + "][" + j + "] must be equal edgesMatrix[" + j + "][" + i + "]");
+                }
+            }
+        }
+
+        this.edgesMatrix = edgesMatrix;
+    }
+
+    public void visitInDeep(Consumer<Integer> consumer) {
+        boolean[] visited = new boolean[edgesMatrix.length];
+        boolean[] addedInStack = new boolean[edgesMatrix.length];
+
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                Deque<Integer> stack = new LinkedList<>();
+                stack.addLast(i);
+                addedInStack[i] = true;
+
+                while (!stack.isEmpty()) {
+                    int currentVertexNumber = stack.removeLast();
+
+                    consumer.accept(currentVertexNumber);
+                    visited[currentVertexNumber] = true;
+
+                    for (int vertexNumber = edgesMatrix[currentVertexNumber].length - 1; vertexNumber >= 0; vertexNumber--) {
+                        if (edgesMatrix[currentVertexNumber][vertexNumber] == 1 && !addedInStack[vertexNumber]) {
+                            stack.add(vertexNumber);
+                            addedInStack[vertexNumber] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void visitInWidth(Consumer<Integer> consumer) {
+        boolean[] visited = new boolean[edgesMatrix.length];
+        boolean[] addedInStack = new boolean[edgesMatrix.length];
+
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                Queue<Integer> stack = new LinkedList<>();
+                stack.add(i);
+                addedInStack[i] = true;
+
+                while (!stack.isEmpty()) {
+                    int currentVertexNumber = stack.remove();
+
+                    consumer.accept(currentVertexNumber);
+                    visited[currentVertexNumber] = true;
+
+                    for (int vertexNumber = 0; vertexNumber < edgesMatrix[currentVertexNumber].length; vertexNumber++) {
+                        if (edgesMatrix[currentVertexNumber][vertexNumber] == 1 && !addedInStack[vertexNumber]) {
+                            stack.add(vertexNumber);
+                            addedInStack[vertexNumber] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void visitInDeepWithRecursion(Consumer<Integer> consumer) {
+        boolean[] visited = new boolean[edgesMatrix.length];
+
+        for (int vertexNumber = 0; vertexNumber < edgesMatrix.length; vertexNumber++) {
+            if (!visited[vertexNumber]) {
+                visitInDeepWithRecursion(vertexNumber, visited, consumer);
+            }
+        }
+    }
+
+    private void visitInDeepWithRecursion(int beginningVertexNumber, boolean[] visited, Consumer<Integer> consumer) {
+        consumer.accept(beginningVertexNumber);
+        visited[beginningVertexNumber] = true;
+
+        for (int vertexNumber = 0; vertexNumber < edgesMatrix[beginningVertexNumber].length; vertexNumber++) {
+            if (edgesMatrix[beginningVertexNumber][vertexNumber] == 1 && !visited[vertexNumber]) {
+                visitInDeepWithRecursion(vertexNumber, visited, consumer);
+            }
+        }
+    }
+}
---
+++ Graph/src/ru/oop/nikolenko/graph_main/MyGraphMain.java
@@ -0,0 +1,61 @@
+package ru.oop.nikolenko.graph_main;
+
+import ru.oop.nikolenko.graph.Graph;
+
+import java.util.function.Consumer;
+
+public class MyGraphMain {
+    public static void main(String[] args) {
+        int[][] edgesMatrix = {
+                {0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1, 1, 0, 0, 0, 0},
+                {0, 1, 0, 1, 0, 1, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 1, 0, 0, 0, 0},
+                {1, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+        };
+
+        Graph graph = new Graph(edgesMatrix);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
+
+        Consumer<Integer> consumer = x -> stringBuilder.append(x).append(", ");
+
+        graph.visitInDeep(consumer);
+
+        if (stringBuilder.length() > 1) {
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}");
+        } else {
+            stringBuilder.append("}");
+        }
+
+        System.out.println(stringBuilder.toString());
+
+        stringBuilder.delete(0, stringBuilder.length()).append("{");
+        graph.visitInDeepWithRecursion(consumer);
+
+        if (stringBuilder.length() > 1) {
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}");
+        } else {
+            stringBuilder.append("}");
+        }
+
+        System.out.println(stringBuilder.toString());
+
+        stringBuilder.delete(0, stringBuilder.length()).append("{");
+        graph.visitInWidth(consumer);
+
+        if (stringBuilder.length() > 1) {
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}");
+        } else {
+            stringBuilder.append("}");
+        }
+
+        System.out.println(stringBuilder.toString());
+    }
+}
